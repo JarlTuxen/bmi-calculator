f06ad0dd64c0f276d506061e1c5b6987b7b1dd58
jarltuxen <jarl.tuxen@gmail.com>
2021-03-01 11:37:48 +0100

    Initial commit

---
+++ src/main/java/kea/dk/bmicalculator/controller/HomeController.java
@@ -0,0 +1,13 @@
+package kea.dk.bmicalculator.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
---
+++ src/test/java/kea/dk/bmicalculator/BmiCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.dk.bmicalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmiCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
